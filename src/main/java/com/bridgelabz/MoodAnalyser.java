8a0d3b6efccd96cf04009aae62e6a9967f4c0601
omkar <omkardudhewar121@gmail.com>
2022-12-24 00:37:54 +0530

    added: mood test should return sad

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+    private  String message;
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+    public String analyseMood() {
+        if (this.message.contains("sad")) {
+            return "SAD";
+        }else
+            return "HAPPY";
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("i am sad");
+        System.out.println(moodAnalyser.analyseMood());
+        MoodAnalyser mood2 = new MoodAnalyser("i am happy");
+        System.out.println(mood2.analyseMood());
+    }
+}
---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,14 @@
+import com.bridgelabz.MoodAnalyser;
+import org.testng.annotations.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class MoodAnalyserTest {
+    @Test
+    public void moodAnalyser() {
+        MoodAnalyser analyse = new MoodAnalyser("I am in sad mood");
+        String mood = analyse.analyseMood();
+        assertSame("SAD", mood);
+    }
+}
