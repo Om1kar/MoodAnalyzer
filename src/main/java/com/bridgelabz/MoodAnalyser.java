3fe60c57c6cfb421b2f737fc83517184319f9793
omkar <omkardudhewar121@gmail.com>
2022-12-25 18:34:38 +0530

    added: code refactored to take the mood message in constructor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,15 +1,29 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-   public String analyseMood(String message) {
-        if (message.contains("sad")) {
-            return "SAD";
-        }else
-            return "HAPPY";
+    private String message;
+
+    public MoodAnalyser() {}
+    /*
+    Refactor->MoodAnalyser will have 2 Constructors –
+              Default - MoodAnalyser() and with
+              Parameters – MoodAnalyser(message)
+     */
+   public MoodAnalyser(String message) {
+            this.message = message;
+        }
+        public String analyseMood () {
+            if (this.message.contains("sad")) {
+                return "SAD";
+            } else
+                return "HAPPY";
+        }
+        public static void main (String[]args){
+            MoodAnalyser moodAnalyser1 = new MoodAnalyser("I am Happy");
+            System.out.println(moodAnalyser1.analyseMood());
+            MoodAnalyser moodAnalyser2 = new MoodAnalyser("I am Sad");
+            System.out.println(moodAnalyser2.analyseMood());
+
+        }
     }
-    public static void main(String[] args) {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        System.out.println(moodAnalyser.analyseMood("I am SAD"));
-        System.out.println(moodAnalyser.analyseMood("I am HAPPY"));
-    }
-}
+
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -3,16 +3,21 @@ import org.testng.annotations.Test;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
 public class MoodAnalyserTest {
+    /*
+    Refactor Method->MoodAnalyser will have 2 Constructors –
+                     Default - MoodAnalyser() and with
+                     Parameters – MoodAnalyser(message)
+     */
     @Test
     public void moodReturnSad() {
-        MoodAnalyser analyse = new MoodAnalyser();
-        String moodAnalyser = analyse.analyseMood("I am in SAD Mood");
+        MoodAnalyser analyse = new MoodAnalyser("I am in SAD Mood");
+        String moodAnalyser = analyse.analyseMood();
         assertSame("SAD", moodAnalyser);
     }
     @Test
     public void moodShouldReturnHappy(){
-        MoodAnalyser analyse = new MoodAnalyser();
-        String moodAnalyser = analyse.analyseMood("I am in any Mood");
+        MoodAnalyser analyse = new MoodAnalyser("I am in Any Mood");
+        String moodAnalyser = analyse.analyseMood();
         assertSame("HAPPY",moodAnalyser);
     }
 }
